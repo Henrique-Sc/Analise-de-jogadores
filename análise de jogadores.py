4bbb2c105a29961dec3bfe7a5bff3fe2141b4184
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-10 11:48:03 -0300

    Update análise de jogadores.py

--- análise de jogadores.py
+++ análise de jogadores.py
@@ -12,7 +12,6 @@ while True:
     print('-' * 38)  # Linha para Design
     jogadores.append({'nome': str(input('Nome do(a) jogador(a): ')).strip().title(), 'gols': list()})
     
-    # noinspection PyTypeChecker
     jogadores[cont]['partidas'] = int(input(f'Quantas partidas {jogadores[cont]["nome"]} jogou? '))
     if jogadores[cont]['partidas'] == 0:
         jogadores[cont]['gols'].append(0)
@@ -65,8 +64,8 @@ sleep(0.5)
 
 # Dados mais detalhados
 print('\n~Digite o código do jogador')
-# While para a inserção dos dados
 
+# While para a inserção dos dados
 while True:
     levant = int(input('\nMostrar dados de qual jogador(a)? (999 para parar) ').strip())
 
